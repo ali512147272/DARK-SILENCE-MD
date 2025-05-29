a83181e24e500d734cf22977d40c6d4790343276
ali512147272 <waqasahmad03240032968@gmail.com>
2025-05-29 21:16:40 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-/*_______________________________________________________________________________________________________________________________________________________________________________________________________________________
+h/*_______________________________________________________________________________________________________________________________________________________________________________________________________________________
 ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────── 
 ─██████████████──██████████──██████──────────██████████████──██████──────────██████──██████████████──██████──────────██████████████──██████──██████──██████████████──████████████████─── 
 ─██░░░░░░░░░░██──██░░░░░░██──██░░██──────────██░░░░░░░░░░██──██░░██████████──██░░██──██░░░░░░░░░░██──██░░██──────────██░░░░░░░░░░██──██░░██──██░░██──██░░░░░░░░░░██──██░░░░░░░░░░░░██─── 
@@ -12,8 +12,8 @@
 ─██░░░░░░░░░░██──██░░░░░░██──██░░░░░░░░░░██──██░░░░░░░░░░██──██░░██──██████████░░██──────██░░██──────██░░░░░░░░░░██──██░░░░░░░░░░██────████░░████────██░░░░░░░░░░██──██░░██──██░░░░░░██─ 
 ─██████████████──██████████──██████████████──██████████████──██████──────────██████──────██████──────██████████████──██████████████──────██████──────██████████████──██████──██████████─ 
 ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────────── 
-created by Silent_lover432 🕵
-contact me 923096287432 ♻️
+created by ⎯꯭̽Ξ𓆩»»»𝙓_𝙙𝙖𝙧𝙠»»»𓆪Ξ⎯꯭̽     𓆩𓃮𓆪  𓆩𓃮𓆪  𓆩𓃮𓆪  〆͎   ⎯꯭̽ꪹ𝑳𝑬𝑮𝑬𝑵𝑫--𝑩𝑶𝑰𝑰⎯꯭ 🕵
+contact me 923360607835 ♻️
 © Copy coder alert ⚠
 */
 
@@ -31,33 +31,32 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "enter your session",
-    CAPTION: process.env.CAPTION || "POWERED BY SILENTLOVER432",
+    SESSION_ID: process.env.SESSION_ID || "DARK-SILENCE-MD~HVhhUJAD#eDvvzuygYRQJXX5RBiuRzTmb-SeYXFFWUNNhTupO4QM",
+    CAPTION: process.env.CAPTION || "POWERED BY ⎯꯭̽Ξ𓆩»»»𝙓_𝙙𝙖𝙧𝙠»»»𓆪Ξ⎯꯭̽     𓆩𓃮𓆪  𓆩𓃮𓆪  𓆩𓃮𓆪  〆͎   ⎯꯭̽ꪹ𝑳𝑬𝑮𝑬𝑵𝑫--𝑩𝑶𝑰𝑰⎯꯭",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     AUTO_REPLY_STATUS: process.env.AUTO_REPLY_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
     MODE: process.env.MODE || "public",
     AUTO_VOICE: process.env.AUTO_VOICE || "true",
-    AUTO_STICKER: process.env.AUTO_STICKER || "false",
-    AUTO_REPLY: process.env.AUTO_REPLY || "false",
+    AUTO_STICKER: process.env.AUTO_STICKER || "true",
+    AUTO_REPLY: process.env.AUTO_REPLY || "true",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Awais-star-a11y/TESTING-REPO/raw/refs/heads/main/IMG-20250505-WA0055.jpg",
-    ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M DARK-SILENCE-MD WHATSAPP BOT 😊♻️",
+    ALIVE_MSG: process.env.ALIVE_MSG || "HII DEAR IM ONLINE I'M ⎯꯭̽Ξ𓆩»»»𝙓_𝙙𝙖𝙧𝙠»»»𓆪Ξ⎯꯭̽     𓆩𓃮𓆪  𓆩𓃮𓆪  𓆩𓃮𓆪  〆͎   ⎯꯭̽ꪹ𝑳𝑬𝑮𝑬𝑵𝑫--𝑩𝑶𝑰𝑰⎯꯭ WHATSAPP BOT 😊♻️",
     ANTI_LINK: process.env.ANTI_LINK || "true",
     ANTI_BAD: process.env.ANTI_BAD || "true",
     PREFIX: process.env.PREFIX || ".",
     FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
-    FAKE_TYPING: process.env.FAKE_TYPING || "false",
+    FAKE_TYPING: process.env.FAKE_TYPING || "true",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
     CURRENT_STATUS: process.env.CURRENT_STATUS || "true",
     AUTO_REACT: process.env.AUTO_REACT || "true",
     HEART_REACT: process.env.HEART_REACT || "true",
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "923096287432",
-    OWNER_NAME: process.env.OWNER_NAME || "➺ѕเℓεɳƭ_ℓσѵε૨࿐",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "923360607835",
+    OWNER_NAME: process.env.OWNER_NAME || "➺darkɳƭ_ℓσѵε૨࿐",
     READ_CMD: process.env.READ_CMD || "true",
-    BOT_NAME: process.env.BOT_NAME || "➺DARK-SILENCE-MD",
-    STATUS_REPLY: process.env.STATUS_REPLY || "`➺үσυя sтαтυs sεεη נυsт ησω вү ∂αяк-sιℓεηcε-м∂`",
+    BOT_NAME: process.env.BOT_NAME || "➺⎯꯭̽Ξ𓆩»»»𝙓_𝙙𝙖𝙧𝙠»»»𓆪Ξ⎯꯭̽     𓆩𓃮𓆪  𓆩𓃮𓆪  𓆩𓃮𓆪  〆͎   ⎯꯭̽ꪹ𝑳𝑬𝑮𝑬𝑵𝑫--𝑩𝑶𝑰𝑰⎯꯭",
+    STATUS_REPLY: process.env.STATUS_REPLY || "`➺үσυя sтαтυs sεεη נυsт ησω вү ⎯꯭̽Ξ𓆩»»»𝙓_𝙙𝙖𝙧𝙠»»»𓆪Ξ⎯꯭̽     𓆩𓃮𓆪  𓆩𓃮𓆪  𓆩𓃮𓆪  〆͎   ⎯꯭̽ꪹ𝑳𝑬𝑮𝑬𝑵𝑫--𝑩𝑶𝑰𝑰⎯꯭`",
     STATUS_REACT: process.env.STATUS_REACT || "true",
-    INBOX_BLOCK: process.env.INBOX_BLOCK || "false",
     ANTI_VV: process.env.ANTI_VV || "true",// true for anti once view 
     ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "same", // change it to 'log' if you want to resend deleted message in ib chat 
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39"// omdbapi.com
